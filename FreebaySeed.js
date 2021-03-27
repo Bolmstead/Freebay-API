a9143f1d1548e421e319d3306a076bc670fe50e0
Berkley Olmstead <olms2074@gmail.com>
2021-03-27 16:20:05 -0600

    playing around with seed file

--- FreebaySeed.js
+++ FreebaySeed.js
@@ -70,54 +70,54 @@ class SeedProducts{
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, valuesArray)
       }
     
-    for (let i = 0; i < products2.length; i++) {
-      console.log(i)
-      const {item, category, sub_category, description, image_1, market_price} = products2[i]
+    // for (let i = 0; i < products2.length; i++) {
+    //   console.log(i)
+    //   const {item, category, sub_category, description, image_1, market_price} = products2[i]
 
-      // Create Random DateTime object
-      let auction_end_dt = SeedProducts.randomDate(new Date(2021, 3, 25), new Date())
-      console.log("auction_end_dt", auction_end_dt)
+    //   // Create Random DateTime object
+    //   let auction_end_dt = SeedProducts.randomDate(new Date(2021, 3, 25), new Date())
+    //   console.log("auction_end_dt", auction_end_dt)
 
-      // Create starting price as half of the product's actual price
-      let starting_bid = market_price * .5
-      starting_bid = Math.round(100*starting_bid)/100
+    //   // Create starting price as half of the product's actual price
+    //   let starting_bid = market_price * .5
+    //   starting_bid = Math.round(100*starting_bid)/100
 
-      // Grab random rating and number of ratings
-      const num_of_ratings = SeedProducts.randomNumberOfRatings()
-      const rating = SeedProducts.randomRating()
-      const condition = SeedProducts.randomCondition()
+    //   // Grab random rating and number of ratings
+    //   const num_of_ratings = SeedProducts.randomNumberOfRatings()
+    //   const rating = SeedProducts.randomRating()
+    //   const condition = SeedProducts.randomCondition()
 
-      const valuesArray =
-        [item, category, sub_category, description, condition, rating, num_of_ratings, image_1, starting_bid, auction_end_dt]
+    //   const valuesArray =
+    //     [item, category, sub_category, description, condition, rating, num_of_ratings, image_1, starting_bid, auction_end_dt]
 
-      await db.query(`INSERT INTO products (name, category, sub_category, description, condition, rating, num_of_ratings, image_url, starting_bid, auction_end_dt) 
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, valuesArray)
-      }
+    //   await db.query(`INSERT INTO products (name, category, sub_category, description, condition, rating, num_of_ratings, image_url, starting_bid, auction_end_dt) 
+    //   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, valuesArray)
+    //   }
 
-    for (let i = 0; i < products3.length; i++) {
-      console.log(i)
-      const {item, category, sub_category, description, image_1, market_price} = products3[i]
+    // for (let i = 0; i < products3.length; i++) {
+    //   console.log(i)
+    //   const {item, category, sub_category, description, image_1, market_price} = products3[i]
 
-      // Create Random DateTime object
-      let auction_end_dt = SeedProducts.randomDate(new Date(2021, 3, 25), new Date())
-      console.log("auction_end_dt", auction_end_dt) 
+    //   // Create Random DateTime object
+    //   let auction_end_dt = SeedProducts.randomDate(new Date(2021, 3, 25), new Date())
+    //   console.log("auction_end_dt", auction_end_dt) 
 
-      // Create starting price as half of the product's actual price
-      let starting_bid = market_price * .5
-      starting_bid = Math.round(100*starting_bid)/100
+    //   // Create starting price as half of the product's actual price
+    //   let starting_bid = market_price * .5
+    //   starting_bid = Math.round(100*starting_bid)/100
 
-      // Grab random rating and number of ratings
-      const num_of_ratings = SeedProducts.randomNumberOfRatings()
-      const rating = SeedProducts.randomRating()
-      const condition = SeedProducts.randomCondition()
+    //   // Grab random rating and number of ratings
+    //   const num_of_ratings = SeedProducts.randomNumberOfRatings()
+    //   const rating = SeedProducts.randomRating()
+    //   const condition = SeedProducts.randomCondition()
 
-      console.log("item", item)
-      const valuesArray =
-        [item, category, sub_category, description, condition, rating, num_of_ratings, image_1, starting_bid, auction_end_dt]
+    //   console.log("item", item)
+    //   const valuesArray =
+    //     [item, category, sub_category, description, condition, rating, num_of_ratings, image_1, starting_bid, auction_end_dt]
 
-      await db.query(`INSERT INTO products (name, category, sub_category, description, condition, rating, num_of_ratings, image_url, starting_bid, auction_end_dt) 
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, valuesArray)
-      }
+    //   await db.query(`INSERT INTO products (name, category, sub_category, description, condition, rating, num_of_ratings, image_url, starting_bid, auction_end_dt) 
+    //   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, valuesArray)
+    //   }
     }
 }
 

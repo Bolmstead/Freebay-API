e9571af2c15a818631441c54fdd720ba23cb312f
Berkley Olmstead <olms2074@gmail.com>
2021-04-10 19:00:44 -0600

    modified seed files

--- SeedProducts.js
+++ SeedProducts.js
@@ -15,70 +15,9 @@ const db = require("./db");
 
 class FreebaySeed{
 
-  static async createTables() {(
-    await db.query(`DROP TABLE IF EXISTS products, users, products_won, bids, notifications;
-
-      CREATE TABLE products (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(1000) NOT NULL,
-        category VARCHAR(100) NOT NULL,
-        sub_category VARCHAR(100) NOT NULL,
-        description TEXT NOT NULL,
-        rating DECIMAL NOT NULL CHECK (rating <= 5.0),
-        num_of_ratings INTEGER NOT NULL,
-        image_url VARCHAR(2083),
-        starting_bid DECIMAL NOT NULL,
-        auction_end_dt TIMESTAMP NOT NULL,
-        auction_ended BOOLEAN DEFAULT false
-      );
-      
-      CREATE TABLE users (
-        email varchar(50) PRIMARY KEY NOT NULL,
-        username VARCHAR(30) UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        first_name VARCHAR(50) NOT NULL,
-        last_name VARCHAR(50) NOT NULL,
-        balance DECIMAL NOT NULL,
-        image_url VARCHAR(2083),
-        last_login TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-      );
-      
-      CREATE TABLE products_won (
-        product_id INTEGER NOT NULL
-          REFERENCES products(id) ON DELETE CASCADE,
-        user_email VARCHAR(50) NOT NULL
-          REFERENCES users(email) ON DELETE CASCADE,
-        bid_price DECIMAL NOT NULL,
-        won_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-      );
-      
-      CREATE TABLE bids (
-        bid_id SERIAL PRIMARY KEY,
-        product_id INTEGER NOT NULL
-          REFERENCES products(id) ON DELETE CASCADE,
-        user_email VARCHAR(50) NOT NULL
-          REFERENCES users(email) ON DELETE CASCADE,
-        bid_price DECIMAL NOT NULL,
-        is_highest_bid BOOLEAN DEFAULT true,
-        was_winning_bid BOOLEAN DEFAULT false,
-        bid_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-      );
-      
-      CREATE TABLE notifications (
-        id SERIAL PRIMARY KEY,
-        user_email VARCHAR(50)
-          REFERENCES users(email) ON DELETE CASCADE,
-        text TEXT,
-        related_product_id INTEGER,
-        was_viewed BOOLEAN DEFAULT false,
-        category VARCHAR(50),
-        notification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-      )`
-  ))}
-
   static randomDate(start, end) {
     let currentDateTime = new Date()
-    return new Date(currentDateTime.getTime() + Math.random() * (259200000));
+    return new Date(currentDateTime.getTime() + Math.random() * (1500000000));
   }
 
   static randomRating() {
@@ -256,7 +195,6 @@ class FreebaySeed{
   }
 }
 
-FreebaySeed.createTables()
 FreebaySeed.seedAllProducts()
 // FreebaySeed.seedSampleUsers()
 

6709d6450dd7545f7fd5d5332833fe681d28fda3
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2023-05-31 11:44:59 -0600

    update db creds

--- config.js
+++ config.js
@@ -9,14 +9,6 @@ const SECRET_KEY = "secret-key333";
 
 const PORT = process.env.PORT || 3001;
 
-// Use dev database, testing database, or via env var, production database
-function getDatabaseUri() {
-  console.log("process.env.DATABASE_URL: ", process.env.DATABASE_URL )
-  return (process.env.NODE_ENV === "test")
-      ? "freebay_test"
-      : process.env.DATABASE_URL || "freebay";
-}
-
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
 // WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
@@ -26,12 +18,11 @@ console.log("freebay Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
+console.log("Database:".yellow, process.env.DATABASE_URL);
 console.log("---");
 
 module.exports = {
   SECRET_KEY,
   PORT,
   BCRYPT_WORK_FACTOR,
-  getDatabaseUri,
 };

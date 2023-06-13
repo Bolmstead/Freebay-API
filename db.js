388a59c002b0449e436fdbadc8341c64dc8a1bdf
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2023-06-13 13:48:12 -0600

    Updated DB code

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { Client } = require("pg");
 
+// console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
+
 const db = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: process.env.HEROKU_DATABASE_URL,
+  password: process.env.HEROKU_DATABASE_PASSWORD,
+  ssl: { rejectUnauthorized: false },
 });
 
 db.connect();

190dcdec60398dcd4adc165d0d44c50d6868c800
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2023-06-13 13:50:09 -0600

    Set up for ElephantSQL

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const { Client } = require("pg");
 // console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
 
 const db = new Client({
-  connectionString: process.env.HEROKU_DATABASE_URL,
-  password: process.env.HEROKU_DATABASE_PASSWORD,
+  connectionString: process.env.ELEPHANT_DATABASE_URL,
+  password: process.env.ELEPHANT_DATABASE_PASSWORD,
   ssl: { rejectUnauthorized: false },
 });
 

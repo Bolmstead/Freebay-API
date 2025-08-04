604840244851a17473b2ba059ecba94d11b50262
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2025-08-03 19:32:28 -0600

    .

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:4200", "https://freebay.netlify.app/"],
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
